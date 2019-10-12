4a5bfe9e6261b1b3765bc358928aa23b34a94c9d
Hugh Cole-Baker <sigmaris@gmail.com>
2019-10-12 13:05:05 +0100

    media: hantro: Fix motion vectors usage condition
    From: Francois Buergisser <fbuergisser@chromium.org>
    
    The setting of the motion vectors usage and the setting of motion
    vectors address are currently done under different conditions.
    
    When decoding pre-recorded videos, this results of leaving the motion
    vectors address unset, resulting in faulty memory accesses. Fix it
    by using the same condition everywhere, which matches the profiles
    that support motion vectors.
    
    Adapted to apply on top of 8ef3e5a3d1e1 ("media: hantro: Refactor G1 H264 code")
    
    Fixes: dea0a82f3d22 ("media: hantro: Add support for H264 decoding on G1")
    Signed-off-by: Francois Buergisser <fbuergisser@chromium.org>
    Signed-off-by: Ezequiel Garcia <ezequiel@collabora.com>
    Signed-off-by: Hugh Cole-Baker <sigmaris@gmail.com>

--- drivers/staging/media/hantro/hantro_g1_h264_dec.c
+++ drivers/staging/media/hantro/hantro_g1_h264_dec.c
@@ -257,7 +257,7 @@ void hantro_g1_h264_dec_run(struct hantro_ctx *ctx)
 	      G1_REG_PIC_TOPFIELD_E(!(slices[0].flags & V4L2_H264_SLICE_FLAG_BOTTOM_FIELD)) |
 	      G1_REG_FILTERING_DIS(0) |
 	      G1_REG_PIC_FIXED_QUANT(0) |
-	      G1_REG_WRITE_MVS_E(dec_param->nal_ref_idc) |
+	      G1_REG_WRITE_MVS_E(sps->profile_idc > 66 && dec_param->nal_ref_idc) |
 	      G1_REG_SEQ_MBAFF_E(sps->flags & V4L2_H264_SPS_FLAG_MB_ADAPTIVE_FRAME_FIELD) |
 	      G1_REG_PICORD_COUNT_E(1) |
 	      G1_REG_DEC_AXI_WR_ID(0);
