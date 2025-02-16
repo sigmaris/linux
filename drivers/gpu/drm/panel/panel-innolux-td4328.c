f64bc0d08657f30db022c23652c01e9c9d577811
Hugh Cole-Baker <sigmaris@gmail.com>
2025-02-16 15:40:42 +0000

    HACK: debugging panel init

--- drivers/gpu/drm/panel/panel-innolux-td4328.c
+++ drivers/gpu/drm/panel/panel-innolux-td4328.c
@@ -38,25 +38,146 @@ static void innolux_td4328_reset(struct innolux_td4328 *ctx)
 	msleep(80);
 }
 
+static int write_te = 0;
+static bool sseq_1 = false;
+static bool sseq_2 = false;
+static bool sseq_3 = false;
+static bool sseq_4 = false;
+static bool sseq_5 = false;
+
+module_param(write_te, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(write_te, "How to write SET_TEAR_ON: 0 = use proper function, 1 = MIPI_DSI_DCS_SHORT_WRITE with no param, 2 = MIPI_DSI_DCS_SHORT_WRITE_PARAM with 0x00");
+module_param(sseq_1, bool, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(sseq_1, "Use the init sequence from the published source instead of from the Android dump");
+module_param(sseq_2, bool, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(sseq_2, "Use the init sequence from the published source instead of from the Android dump");
+module_param(sseq_3, bool, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(sseq_3, "Use the init sequence from the published source instead of from the Android dump");
+module_param(sseq_4, bool, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(sseq_4, "Use the init sequence from the published source instead of from the Android dump");
+module_param(sseq_5, bool, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(sseq_5, "Use the init sequence from the published source instead of from the Android dump");
+
 static int innolux_td4328_enable(struct drm_panel *panel)
 {
 	struct innolux_td4328 *ctx = to_innolux_td4328(panel);
 	struct mipi_dsi_multi_context dsi_ctx = { .dsi = ctx->dsi };
+	struct device *dev = &ctx->dsi->dev;
 
 	ctx->dsi->mode_flags |= MIPI_DSI_MODE_LPM;
 
+	/* 
+	Original init sequence from https://github.com/AYN-Tech/odin_android_kernel/blob/master/arch/arm64/boot/dts/qcom/dsi-panel-innolux-td4328-1080p-cmd.dtsi#L61
+	  |  |  |  |  |     |    P
+	  |  |  |  |  |     |    A
+	D |  |  |  |  |     |    Y
+	T |L |  |  |W |  D  |    L
+	Y |A |  |A |A |  L  |    O
+	P |S |V |C |I |  E  |    A
+	E |T |C |K |T |  N  |    D
+	// 29 = MIPI_DSI_GENERIC_LONG_WRITE
+	29 01 00 00 00 00 02 B0 00
+	29 01 00 00 00 00 18 C2 
+	                     01 F7 80 08 68 08
+	                     0C 10 00 08 70 00
+	                     00 00 00 00 00 00
+	                     03 83 00 00 00
+	29 01 00 00 00 00 02 D6 01
+	29 01 00 00 00 00 02 B0 03
+	// 39 = MIPI_DSI_DCS_LONG_WRITE
+	39 01 00 00 00 00 05 2A 00 00 04 37
+	39 01 00 00 00 00 05 2B 00 00 07 7F
+	// 05 = MIPI_DSI_DCS_SHORT_WRITE
+	05 01 00 00 00 00 02 35 00
+	05 01 00 00 96 00 01 11
+	05 01 00 00 32 00 01 29
+
+	Android DT-dump init sequence:
+	  |  |  |  |  |     |    P
+	  |  |  |  |  |     |    A
+	D |  |  |  |  |     |    Y
+	T |L |  |  |W |  D  |    L
+	Y |A |  |A |A |  L  |    O
+	P |S |V |C |I |  E  |    A
+	E |T |C |K |T |  N  |    D
+	// 29 = MIPI_DSI_GENERIC_LONG_WRITE
+	29 01 00 00 00 00 02 b0 00
+	29 01 00 00 00 00 18 c2
+	                     01 f7 80 04 68 08
+	                     09 10 00 08 30 00
+	                     00 00 00 00 00 00
+	                     02 80 00 00 00
+	29 01 00 00 00 00 02 d6 01
+	29 01 00 00 00 00 02 b0 03
+	// 39 = MIPI_DSI_DCS_LONG_WRITE
+	39 01 00 00 00 00 05 2a 00 00 04 37
+	39 01 00 00 00 00 05 2b 00 00 07 7f
+	// 05 = MIPI_DSI_DCS_SHORT_WRITE
+	05 01 00 00 00 00 02 35 00
+	05 01 00 00 96 00 01 11
+	05 01 00 00 32 00 01 29
+	*/
+	u8 init_payload[0x18] = {
+		0xc2, 0x01, 0xf7, 0x80,
+		sseq_1 ? 0x08 : 0x04, // Unknown effect
+		0x68, 0x08,
+		sseq_2 ? 0x0c : 0x09, // 0x0c causes wrong colors, RGB becomes YCM
+		0x10, 0x00, 0x08,
+		sseq_3 ? 0x70 : 0x30, // Unknown effect
+		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+		sseq_4 ? 0x03 : 0x02, // 0x03 makes framerate and init reliability consistent
+		sseq_5 ? 0x83 : 0x80, // Unknown effect
+		0x00, 0x00, 0x00
+	};
+	dev_info(dev, "init_payload = %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x %x\n",
+		init_payload[0],
+		init_payload[1],
+		init_payload[2],
+		init_payload[3],
+		init_payload[4],
+		init_payload[5],
+		init_payload[6],
+		init_payload[7],
+		init_payload[8],
+		init_payload[9],
+		init_payload[10],
+		init_payload[11],
+		init_payload[12],
+		init_payload[13],
+		init_payload[14],
+		init_payload[15],
+		init_payload[16],
+		init_payload[17],
+		init_payload[18],
+		init_payload[19],
+		init_payload[20],
+		init_payload[21],
+		init_payload[22],
+		init_payload[23]
+	);
 	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xb0, 0x00);
-	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xc2,
-					 0x01, 0xf7, 0x80, 0x04, 0x68, 0x08,
-					 0x09, 0x10, 0x00, 0x08, 0x30, 0x00,
-					 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-					 0x02, 0x80, 0x00, 0x00, 0x00);
+	mipi_dsi_generic_write_multi(&dsi_ctx, init_payload, ARRAY_SIZE(init_payload));
 	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xd6, 0x01);
 	mipi_dsi_generic_write_seq_multi(&dsi_ctx, 0xb0, 0x03);
 
 	mipi_dsi_dcs_set_column_address_multi(&dsi_ctx, 0, 1080 - 1);
 	mipi_dsi_dcs_set_page_address_multi(&dsi_ctx, 0, 1920 - 1);
-	mipi_dsi_dcs_set_tear_on_multi(&dsi_ctx, MIPI_DSI_DCS_TEAR_MODE_VBLANK);
+	switch (write_te) {
+		case 1:
+			dev_info(dev, "Set tear on with MIPI_DSI_DCS_SHORT_WRITE\n");
+			u8 d1[1] = { 0x35 };
+			mipi_dsi_dcs_write_buffer_multi(&dsi_ctx, d1, 1);
+			break;
+		case 2:
+			dev_info(dev, "Set tear on with MIPI_DSI_DCS_SHORT_WRITE_PARAM\n");
+			u8 d2[2] = { 0x35, 0x00 };
+			mipi_dsi_dcs_write_buffer_multi(&dsi_ctx, d2, 2);
+			break;
+		default:
+			dev_info(dev, "Set tear on with function\n");
+			mipi_dsi_dcs_set_tear_on_multi(&dsi_ctx, MIPI_DSI_DCS_TEAR_MODE_VBLANK);
+			break;
+	}
 	mipi_dsi_dcs_exit_sleep_mode_multi(&dsi_ctx);
 	mipi_dsi_msleep(&dsi_ctx, 150);
 	mipi_dsi_dcs_set_display_on_multi(&dsi_ctx);
