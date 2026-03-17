a77a18d69a77275d008697a3995116892a6d8f73
Sebastian Reichel <sebastian.reichel@collabora.com>
2026-03-17 15:43:25 +0100

    [DEBUG] usb: typec: altmode/displayport: print message on probe error
    Print an error message when the DisplayPort AltMode driver probe failed
    due to incorrect pin assignment or wrong data-role to ease debugging
    issues.
    
    Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

--- drivers/usb/typec/altmodes/displayport.c
+++ drivers/usb/typec/altmodes/displayport.c
@@ -764,8 +764,10 @@ int dp_altmode_probe(struct typec_altmode *alt)
 	struct dp_altmode *dp;
 
 	/* Port can only be DFP_U. */
-	if (typec_altmode_get_data_role(alt) != TYPEC_HOST)
+	if (typec_altmode_get_data_role(alt) != TYPEC_HOST) {
+		dev_err(alt->dev.parent->parent, "Cannot probe DP AltMode as data-role is not HOST\n");
 		return -EPROTO;
+	}
 
 	/* Make sure we have compatible pin configurations */
 	if (!(DP_CAP_PIN_ASSIGN_DFP_D(port->vdo) &
@@ -773,6 +775,7 @@ int dp_altmode_probe(struct typec_altmode *alt)
 	    !(DP_CAP_PIN_ASSIGN_UFP_D(port->vdo) &
 	      DP_CAP_PIN_ASSIGN_DFP_D(alt->vdo))) {
 		typec_altmode_put_plug(plug);
+		dev_err(alt->dev.parent->parent, "Cannot probe DP AltMode due to incorrect pin assignment\n");
 		return -ENODEV;
 	}
 
